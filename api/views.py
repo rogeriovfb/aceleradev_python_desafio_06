9d3a8ece29f0bebdde1493b57d447243b9ae9fc8
Rogerio Ballestrin <ballestrin.rogerio@gmail.com>
2020-06-21 08:57:14 -0300

    Implementando lambda_function

--- api/views.py
+++ api/views.py
@@ -1,5 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from collections import Counter
 
 
+@api_view(['POST'])
 def lambda_function(request):
-    raise NotImplementedError
-
+    data = request.data.get('question')
+    saida = []
+    for valor, quantidade in Counter(data).most_common(len(Counter(data))):
+        for _ in range(0, quantidade):
+            saida.append(valor)
+    return Response({'solution': saida})
--- restapi/api/views.py
+++ restapi/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from collections import Counter
 
 
 @api_view(['POST'])
 def lambda_function(request):
     data = request.data.get('question')
-    print(data)
-    return Response({'1': '2'})
-
+    saida = []
+    for valor, quantidade in Counter(data).most_common(len(Counter(data))):
+        for _ in range(0, quantidade):
+            saida.append(valor)
+    return Response({'solution': saida})
